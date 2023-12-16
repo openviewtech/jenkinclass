49d7443061124a673ab4cce46a5dbd63e5e15ea8
Javier Avalos <43050682+openviewtech@users.noreply.github.com>
2023-12-15 22:15:28 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 15122023-v1.0.0 CI/CD in AKS v1.0.0 JENKINS";
+    return "GET - Net 6 Running ... 15122023-v1.0.0 CI/CD in AKS v1.0.0 JENKINS-V2";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ...15122023-v1.0.0 CI/CD in AKS v1.0.0 JENKINS";
+    return "POST - Net 6 Running ...15122023-v1.0.0 CI/CD in AKS v1.0.0 JENKINS-V2";
 });
 
 //app.Run($"http://0.0.0.0:{port}");
